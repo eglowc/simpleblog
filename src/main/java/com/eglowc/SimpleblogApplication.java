5a686af2ff313d4ee77ee24ff9abc40fc1020d4f
eglowc <eglowc@gmail.com>
2016-10-07 18:09:55 +0900

    JSR-310 스펙 지원설정 추가

--- src/main/java/com/eglowc/SimpleblogApplication.java
+++ src/main/java/com/eglowc/SimpleblogApplication.java
@@ -1,12 +1,22 @@
 package com.eglowc;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {SimpleblogApplication.class, Jsr310JpaConverters.class} )
 public class SimpleblogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleblogApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelmapper() {
+		return new ModelMapper();
 	}
 }
