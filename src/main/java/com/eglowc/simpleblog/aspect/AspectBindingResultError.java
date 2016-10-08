b3e5f2ca2ddbc3cc5ecafe18788ddb27fcfa73af
eglowc <eglowc@gmail.com>
2016-10-08 16:20:26 +0900

    Pointcut 분리 + 더 명시적인 이름 사용

---
+++ src/main/java/com/eglowc/simpleblog/aspect/AspectBindingResultError.java
@@ -0,0 +1,39 @@
+package com.eglowc.simpleblog.aspect;
+
+import com.eglowc.simpleblog.exception.BindingResultErrorUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.Arrays;
+
+/**
+ * @author eglowc<eglowc@gmail.com>
+ */
+@Component
+@Aspect
+@Slf4j
+public class AspectBindingResultError {
+
+    @Pointcut("execution(* com..*Controller.*(..))")
+    public void methodInControllers() {
+    }
+
+    @Pointcut("args(*, result, ..)")
+    public void hasBindingResultArg(BindingResult result) {
+    }
+
+    @Before("methodInControllers() && hasBindingResultArg(result)")
+    public void aspectCheckBindingResultError(JoinPoint joinPoint, BindingResult result) {
+        Arrays.stream(joinPoint.getArgs())
+                .filter(arg -> arg instanceof BindingResult)
+                .findFirst()
+                .map(arg -> (BindingResult) arg)
+                .ifPresent(BindingResultErrorUtil::checkBindingResultError);
+    }
+
+}
